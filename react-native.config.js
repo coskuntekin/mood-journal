6df32d94f47ccb127d848d8fabb5833bac2c954e
coskuntekin <coskuntekin@msn.com>
2024-10-06 10:49:28 +0300

    Add react-native-sqlite-storage dependency to react-native.config.js

---
+++ react-native.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  dependencies: {
+    'react-native-sqlite-storage': {
+      platforms: {
+        ios: {
+          podspecPath:
+            'node_modules/react-native-sqlite-storage/react-native-sqlite-storage.podspec',
+        },
+      },
+    },
+  },
+};
