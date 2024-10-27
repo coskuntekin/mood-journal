1d83c00f4ea83d6ac4ca5d8b34466d396ed2a5ed
coskuntekin <mail@coskuntekin.com>
2024-10-27 23:18:02 +0300

    Refactor database module to add calendar start day to user table and export database data

--- lib/database.ts
+++ lib/database.ts
@@ -5,14 +5,17 @@ const db = SQLite.openDatabase({name: 'mood_journal.db', location: 'default'});
 
 // Create the tables
 db.transaction((tx: SQLite.Transaction) => {
+  // Create the users table
   tx.executeSql(
     `CREATE TABLE IF NOT EXISTS users (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
       username TEXT NOT NULL,
-      profileImage TEXT
+      profileImage TEXT,
+      calendarStartDay TEXT NOT NULL DEFAULT '0'
     );`,
   );
 
+  // Create the moods table
   tx.executeSql(
     `CREATE TABLE IF NOT EXISTS moods (
       id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -27,12 +30,39 @@ db.transaction((tx: SQLite.Transaction) => {
 });
 
 // Insert a new user
-const insertUser = async (username: string, profileImage: string) => {
+const insertUser = async (
+  username: string,
+  profileImage: string,
+  calendarStartDay: string,
+) => {
   (await db).transaction((tx: SQLite.Transaction) => {
-    tx.executeSql('INSERT INTO users (username, profileImage) VALUES (?, ?)', [
-      username,
-      profileImage,
-    ]);
+    tx.executeSql(
+      'INSERT INTO users (username, profileImage, calendarStartDay) VALUES (?, ?, ?)',
+      [username, profileImage, calendarStartDay],
+    );
+  });
+};
+
+const updateUserSettings = async (
+  userId: number,
+  username: string,
+  profileImage: string,
+  calendarStartDay: string,
+) => {
+  return new Promise<void>(async (resolve, reject) => {
+    (await db).transaction((tx: SQLite.Transaction) => {
+      tx.executeSql(
+        'UPDATE users SET username = ?, profileImage = ?, calendarStartDay = ? WHERE id = ?',
+        [username, profileImage, calendarStartDay, userId],
+        (_, results) => {
+          if (results.rowsAffected > 0) {
+            resolve();
+          } else {
+            reject(new Error('Failed to update user settings'));
+          }
+        },
+      );
+    });
   });
 };
 
@@ -51,25 +81,17 @@ const insertMood = async (
   });
 };
 
-const updateMood = async (moodId: number, moodEmoji: string) => {
-  (await db).transaction(tx => {
-    tx.executeSql('UPDATE moods SET moodEmoji = ? WHERE id = ?', [
-      moodEmoji,
-      moodId,
-    ]);
-  });
-};
-
 const getUserProfile = (userId: number) => {
-  return new Promise((resolve, reject) => {
-    db.transaction(tx => {
+  return new Promise(async (resolve, reject) => {
+    (await db).transaction((tx: SQLite.Transaction) => {
       tx.executeSql(
-        'SELECT username, profileImage FROM users WHERE id = ?',
+        'SELECT username, profileImage, calendarStartDay FROM users WHERE id = ?',
         [userId],
         (tx, results) => {
           if (results.rows.length > 0) {
-            const {username, profileImage} = results.rows.item(0);
-            resolve({username, profileImage});
+            const {username, profileImage, calendarStartDay} =
+              results.rows.item(0);
+            resolve({username, profileImage, calendarStartDay});
           } else {
             reject(new Error('User not found'));
           }
@@ -117,10 +139,54 @@ const getUserMoodsByDate = async (userId: number, date: string) => {
   });
 };
 
+const exportDatabaseData = async () => {
+  return new Promise(async (resolve, _) => {
+    const userData = await new Promise(async (resolve, reject) => {
+      (await db).transaction(tx => {
+        tx.executeSql(
+          'SELECT id, username, profileImage FROM users',
+          [],
+          (__, results) => {
+            const users = [];
+            for (let i = 0; i < results.rows.length; i++) {
+              const {id, username, profileImage} = results.rows.item(i);
+              users.push({id, username, profileImage});
+            }
+            resolve(users);
+          },
+          error => reject(error),
+        );
+      });
+    });
+
+    const moodData = await new Promise(async (resolve, reject) => {
+      (await db).transaction(tx => {
+        tx.executeSql(
+          'SELECT userId, emoji, date, time, note FROM moods',
+          [],
+          (__, results) => {
+            const moods = [];
+            for (let i = 0; i < results.rows.length; i++) {
+              const {userId, emoji, date, time, note} = results.rows.item(i);
+              moods.push({userId, emoji, date, time, note});
+            }
+            resolve(moods);
+          },
+          error => reject(error),
+        );
+      });
+    });
+
+    resolve({users: userData, moods: moodData});
+  });
+};
+
 export {
   insertUser,
   insertMood,
   getUserProfile,
   getUserMoods,
+  updateUserSettings,
   getUserMoodsByDate,
+  exportDatabaseData,
 };
